13439670ded122018886b07d1149a51338073b80
Liem Phan <z5419546@student.unsw.edu.au>
2026-01-22 17:13:40 +1100

    progress: working on flipping card for settings.

--- client/src/pages/component/home_components/FriendBlock.tsx
+++ client/src/pages/component/home_components/FriendBlock.tsx
@@ -1,4 +1,6 @@
 // Changes colour based on how long ago.
+import { useState } from 'react';
+import FlipButton from './FlipButton';
 
 // 1- 1 week --> GREEN
 // 1 week - 1 month --> YELLOW
@@ -10,12 +12,29 @@ type prop = {
 }
 
 function FriendBlock({name, last_seen}: prop) {
+    const [ flipped, flipOver ] = useState(false);
+
     return <>
-        <div className="w-4/5 h-1/11 ml-5 mr-5 flex flex-col items-center bg-[#F1EDFF] border-2 relative">    
-            <div className="flex justify-center items-center"> <div className="text-xl"> {name} </div>  <div className="bg-red-500 min-w-[1.25vw] min-h-[2.5vh] rounded-full border-red-200 border-2 absolute left-[9.75vw]"></div> </div>
-            <div> Last Seen {last_seen} </div>
-        </div>
-    
+        {!flipped ? (
+            <div className="w-4/5 h-1/11 ml-5 mr-5 flex flex-col items-center bg-[#F1EDFF] border-2 relative rotate-y-0">   
+                <div className="flex justify-center items-center"> 
+                    <FlipButton flipped={flipped} flipOver={flipOver}/>
+                    <div className="text-xl">
+                        {name}
+                    </div>  
+                    <div className="bg-red-500 min-w-[1.25vw] min-h-[2.5vh] rounded-full border-red-200 border-2 absolute right-1 top-0.5"/> 
+                </div>
+                <div>
+                    Last Seen {last_seen} 
+                </div>
+            </div>) :
+            <div className="w-4/5 h-1/11 ml-5 mr-5 flex flex-col items-center bg-[#F1EDFF] border-2 relative rotate-y-180"> 
+               <div className="flex justify-center items-center"> 
+                    <FlipButton flipped={flipped} flipOver={flipOver}/>
+                    YO GABBA GABBA
+                </div>
+            </div>
+        }
     </>
 }
 
