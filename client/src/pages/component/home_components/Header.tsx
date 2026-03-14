b4272a474d33fa3fdd07b3d48e8753b78c771feb
Liem <liemphan802@gmail.com>
2026-03-14 11:05:57 +1100

    chore: front-end revamp for create group.

--- client/src/pages/component/home_components/Header.tsx
+++ client/src/pages/component/home_components/Header.tsx
@@ -3,7 +3,7 @@ import Logout from "./Logout";
 function Header() {
     return (
         <div className="top-0 z-50 w-full bg-linear-to-b from-[#F5ECD7] to-[#ebdfc4] flex items-center justify-between pt-4"> 
-            <div className="bg-linear-to-r to-[#3B1F0E] via-[#3B1F0E] from-[#F5ECD7] text-transparent bg-clip-text ml-[5vw] text-[2.5vw] font-bold bg-size-[200%] animate-colour-wave"> Sisyphus Scheduler </div>           
+            <div className="bg-linear-to-r to-[#3B1F0E] via-[#3B1F0E] from-[#F5ECD7] text-transparent bg-clip-text ml-[5vw] text-[clamp(2rem,2.5vw,5rem)] font-bold bg-size-[200%] animate-colour-wave"> Sisyphus Scheduler </div>           
             <Logout/>
         </div>
     )
