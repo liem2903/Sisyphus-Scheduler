e00eb5920b3d63db5528eb04318f2e1105d1e866
Liem Phan <z5419546@student.unsw.edu.au>
2026-04-03 20:42:33 +1100

    fix: fixed bug where logout wasn't working after 15 minutes.

--- client/src/pages/component/home_components/Logout.tsx
+++ client/src/pages/component/home_components/Logout.tsx
@@ -1,22 +1,19 @@
-import axios from "axios";
 import useAuth from "../../../context/useAuth";
 import { useState } from 'react'
+import { api } from "../../../interceptor/interceptor";
 
 function Logout () {
     const {setStatus } = useAuth();
-
     const [ loggingOut, logOut ] = useState(false);
 
-
     const handleClick = async () => {
-        logOut(true);
         // Delete refresh and access cookies.
-        await axios.get('/api/auth/refresh/logout', { withCredentials: true });
+        await api.get('/api/auth/refresh/logout');
         setStatus("unauthenticated");
     }
     
     return (
-        <button className={["text-lg mr-18 text-[#FFF8F0] font-bold mt-3 pr-3 pl-3 pb-2 pt-2 bg-[#4A7C59] rounded-[4vw] shadow-[0_4px_40px_0_rgba(0,0,0,0.3)]", loggingOut ? "animate-pulse bg-[#4A7C59]/70" : "hover:scale-110 hover:cursor-pointer hover:ring-2 hover:ring-[#4A7C59]/40 hover:ring-offset-2 hover:ring-offset-black/40 transition duration-200"].join(" ")} disabled={loggingOut} onClick={handleClick}> 
+        <button className={["text-lg mr-18 text-[#FFF8F0] font-bold mt-3 pr-3 pl-3 pb-2 pt-2 bg-[#4A7C59] rounded-[4vw] shadow-[0_4px_40px_0_rgba(0,0,0,0.3)]", loggingOut ? "animate-pulse bg-[#4A7C59]/70" : "hover:scale-110 hover:cursor-pointer hover:ring-2 hover:ring-[#4A7C59]/40 hover:ring-offset-2 hover:ring-offset-black/40 transition duration-200"].join(" ")} disabled={loggingOut} onClick={() => {logOut(true); handleClick()}}> 
             {loggingOut ? "Logging Out..." : "Logout"} 
         </button>
     )
